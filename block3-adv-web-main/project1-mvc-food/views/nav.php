7a6fffd5db2a33a865c72e0f0beeafe0fd6bc668
jessica-wu0907 <jessicawu740@gmail.com>
2024-01-17 18:59:38 -0500

    update

--- block3-adv-web-main/project1-mvc-food/views/nav.php
+++ block3-adv-web-main/project1-mvc-food/views/nav.php
@@ -1,52 +1,52 @@
 <div pt-5>
   <div class="container">
     <div class="" style="padding: 100px">
-      <h1 style="text-align:center">MVC project</h1>
-      <a href="?" class="btn">Dishes</a>
-      <a href="?" class="btn">Ingredients</a>
-      <a href="?" class="btn">Suppliers</a>
+     
+       <div class="" style="padding: 100px">
+      <h1 style="text-align:center">Food MVC project</h1>
+     <a href="?page=dishes" class="btn">Dishes</a>
+      <a href="?page=ingredients" class="btn">Ingredients</a >
+      <a href="?page=suppliers" class="btn">Suppliers</a >
     </div>
 
 
     <div class="row mb-5">
       <div class="col-md-4 col-sm-12 mb-3 mb-sm-0">
-        <div class="card h-100 bg-dark text-light">
+        <div class="card h-100  text-dark">
           <div class="card-body">
             <h5 class="card-title">Dishes</h5>
-            <p class="card-text">Adding and displaying all the dishes of the restaurant</p>
+            
 
             <div class="d-flex justify-content-between">
-            <a href="?page=dishes" class="btn" style="background-color: orange; color: white">Check</a>
-              <a href="?page=adddish" class="btn" style="background-color: green; color: white">Add</a>
+           
+              <a href="?page=adddish" class="btn btn-danger">Add</a>
             </div>
 
           </div>
         </div>
       </div>
 
-      <div class="col-md-4 col-sm-12">
-        <div class="card h-100 bg-dark text-light">
-          <div class="card-body">
-            <h5 class="card-title">Ingredients</h5>
-            <p class="card-text">Adding and displaying all the ingredients for preparing the
-              dishes</p>
-            <div class="d-flex justify-content-between">
-            <a href="?page=dishes" class="btn" style="background-color: orange; color: white">Check</a>
-              <a href="?page=adddish" class="btn" style="background-color: green; color: white">Add</a>
-            </div>
-          </div>
-        </div>
-      </div>
+			  <div class="col-md-4 col-sm-12">
+		<div class="card h-100">
+		<div class="card-body">
+		<h5 class="card-title">Ingredients</h5>
+		
+		<div class="d-flex justify-content-between">
+		
+		<a href="?page=addingredient" class="btn btn-danger">Add</a>
+		</div>
+		</div>
+		</div>
+		</div>
 
       <div class="col-md-4 col-sm-12">
-        <div class="card h-100 bg-dark text-light">
+        <div class="card h-100">
           <div class="card-body">
             <h5 class="card-title">Suppliers</h5>
-            <p class="card-text">Adding and dispaying all the suppliers that
-              supply the ingredients for the restaurant</p>
+            
             <div class="d-flex justify-content-between">
-            <a href="?page=dishes" class="btn" style="background-color: orange; color: white">Check</a>
-              <a href="?page=adddish" class="btn" style="background-color: green; color: white">Add</a>
+              
+              <a href="?page=addsupplier" class="btn btn-danger">Add</a >
             </div>
           </div>
         </div>
